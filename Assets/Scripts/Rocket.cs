fcbebe276c56b699e99cc5a2175895ea34099bc0
Nathaniel O'Brien <obrienn.88@gmail.com>
2018-08-09 20:23:57 -0400

    New rocketship with audio and configurable thrust values. Basic tags added, including rudimentary fuel collection (no mechanic for fuel consumption or tracking).

--- Assets/Scripts/Rocket.cs
+++ Assets/Scripts/Rocket.cs
@@ -7,6 +7,8 @@ public class Rocket : MonoBehaviour {
 
     Rigidbody rigidBody;
     AudioSource audioSource;
+    [SerializeField] float rcsThrust = 100f;
+    [SerializeField] float mainThrust = 100f;
 
 	// Use this for initialization
 	void Start () {
@@ -21,19 +23,61 @@ public class Rocket : MonoBehaviour {
 
     void ProcessInput()
     {
+        Thrust();
+        Rotate();
+    }
+
+    private void Thrust()
+    {
         if (Input.GetKey(KeyCode.Space)) // can thrust while rotating
         {
-            rigidBody.AddRelativeForce(Vector3.up);
-            audioSource.Play();
+            rigidBody.AddRelativeForce(Vector3.up * mainThrust);
+            if (!audioSource.isPlaying) // prevents layering
+            {
+                audioSource.Play();
+            }
         }
+        else
+        {
+            audioSource.Stop();
+        }
+    }
+
+    private void Rotate()
+    {
+        rigidBody.freezeRotation = true; // take manual control of rotation
+        float rotationThisFrame = rcsThrust * Time.deltaTime;
 
         if (Input.GetKey(KeyCode.A))
         {
-            transform.Rotate(Vector3.forward);
+            transform.Rotate(Vector3.forward * rotationThisFrame);
         }
         else if (Input.GetKey(KeyCode.D))
         {
-            transform.Rotate(-Vector3.forward);
+            transform.Rotate(-Vector3.forward * rotationThisFrame);
+        }
+
+        rigidBody.freezeRotation = false; // resume physics control of rotation
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        switch (collision.gameObject.tag)
+        {
+            case "Friendly":
+                // do nothing
+                print("Friendly");
+                break;
+            case "Dead":
+                print("You're dead fool");
+                break;
+            case "Fuel":
+                print("You collected fuel");
+                collision.gameObject.SetActive(false);
+                break;
+            default:
+                // do nothing
+                break;
         }
     }
 }
